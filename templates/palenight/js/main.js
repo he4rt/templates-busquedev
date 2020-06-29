438e1c3ef1f3ef59bec8fb053d27355afd58d55f
Matheus Felipe <matheuscorreia559@gmail.com>
2020-06-29 15:14:25 -0400

    add responsiveness

--- templates/palenight/js/main.js
+++ templates/palenight/js/main.js
@@ -1,66 +1,75 @@
 let run = async () => {
-  const data = await fetch("../content.json").then((data) => data.json());
-  console.log(data);
+	const data = await fetch("../content.json").then((data) => data.json());
+	console.log(data);
 
-  document.getElementById("name").innerHTML = data.name;
-  document.getElementById("title").innerHTML = data.title;
-  document.getElementById("about").innerHTML = data.about;
-  document.getElementById("avatar").innerHTML = data.avatar;
+	document.getElementById("name").innerHTML = data.name;
+	document.getElementById("title").innerHTML = data.title;
+	document.getElementById("about").innerHTML = data.about;
+	document.getElementById("avatar").src = data.avatar;
 
-  data.skills.forEach((skill) => {
-    document
-      .getElementById("skills")
-      .insertAdjacentHTML("beforeend", generateSkill(skill));
-  });
-  data.jobs.forEach((job) => {
-    document
-      .getElementById("jobs")
-      .insertAdjacentHTML("beforeend", generateWorkplace(job));
-  });
-  data.socials.forEach((social) => {
-    document
-      .getElementById("socialNetworks")
-      .insertAdjacentHTML("beforeend", generateNetwork(social));
-  });
+	data.skills.forEach((skill) => {
+		document
+			.getElementById("grid-skills")
+			.insertAdjacentHTML("beforeend", generateSkill(skill));
+	});
+	data.jobs.forEach((job) => {
+		document
+			.getElementById("second-ctn")
+			.insertAdjacentHTML("beforeend", generateWorkplace(job));
+	});
+
+	//pra evitar que fique um gap no layout
+	if (data.jobs.length === 0) {
+		document.getElementById("second-ctn").insertAdjacentHTML(
+			"beforeend",
+			generateWorkplace({
+				company: "Nenhum emprego",
+				description: "Ainda :}",
+				worked_time: "0 days",
+			})
+		);
+	}
+
+	data.socials.forEach((social) => {
+		document
+			.getElementById("grid-icons")
+			.insertAdjacentHTML("beforeend", generateNetwork(social));
+	});
 };
 
 let generateWorkplace = (place) => {
-  return `
-    <div class="col-sm-12 timeline-info">
-        <div class="timeline-time">
-            <small>Empresa:</small>
-            <h4>${place.company}</h4>
-            <small>Duração:</small>
-            <h5 class="base-purple">${place.worked_time}</h5>
-        </div>
-        <div class="timeline-description">
-            <p class="pt-md-2">
-            <strong>Descrição:</strong>
-            ${place.description}
-            </p>
-        </div>
-    </div>
+	return `
+	<div class="company-ctn">
+					<div class="company">
+						<div class="up-label">
+							<h2>${place.company}</h2>
+						</div>
+						<p>${place.description}</p>
+						<div class="down-label">
+							<h2>${place.worked_time}</h2>
+						</div>
+					</div>
+				</div>
     `;
 };
 
 let generateSkill = (skill) => {
-  return `
-    <span
-        class="badge badge-pill badge-${skill.type} badge-custom"
-        data-toggle="tooltip"
-        data-placement="bottom"
-        title="${skill.name}<br> ${skill.comment}"
-        data-html="true"
-        >${skill.name}</span
-    >
+	return `
+	<div class="skill" title="${skill.type}">
+		<div class="up-ctn">
+			<h3>${skill.name}</h3>
+		</div>
+		<div class="down-ctn">
+			<p>${skill.time}</p>
+		</div>
+	</div>
     `;
 };
 
 let generateNetwork = (network) => {
-  return `
-    <a href="${network.link}" class="social-links" target="_blank">
+	return `
+    <a href="${network.link}" class="social-links" target="_blank" title="${network.type}">
         <i class="fa ${network.faIcon}" aria-hidden="true"></i>
-        <label>Me siga no ${network.type}</label>
     </a>
     `;
 };
